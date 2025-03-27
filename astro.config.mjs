253376d414452b9946577045161e88c9f2f43ede
Stijn van der Wielen <stijnvanderwielen@gmail.com>
2025-03-27 18:45:43 +0100

    Missing css on production

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://stijnvanderwielen.be',
-  base: 'personal-site'
+  base: ''
 });
