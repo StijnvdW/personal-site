d4959224da7c127673ebe3c5baef28bfaee5316d
Stijn van der Wielen <stijnvanderwielen@gmail.com>
2026-03-26 22:12:39 +0100

    Packages and astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://www.stijnvanderwielen.be',
-  base: ''
+  integrations: [],
+  site: "https://www.stijnvanderwielen.be",
+  base: "",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
